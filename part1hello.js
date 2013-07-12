38a8812381128d01970a828a3c7b36db244279c9
Ubuntu <ubuntu@ip-10-164-61-31.ec2.internal>
2013-07-12 05:43:52 +0000

    part1hello.js

--- part1hello.js
+++ part1hello.js
@@ -1,6 +1,6 @@
 1 #!/usr/bin/env node
 2 var fs = require(’fs’);
-3 var outfile = "hello.txt";
-4 var out = "Hello World 2"
+3 var outfile = "hello2.txt";
+4 var out = "Hello World 2 \n";
 5 fs.writeFileSync(outfile, out);
 6 console.log("Script: " + __filename + "\nWrote: " + out + "To: " + outfile);
